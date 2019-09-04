c09d0ff5961825a60f4803f0070edbded907ced3
Craig Newton <craig@hyvemobile.co.za>
2019-09-04 14:43:09 +0200

    added client with rest template. closes #2

---
+++ src/test/java/com/newtondev/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package com.newtondev.msscbreweryclient.web.client;
+
+import com.newtondev.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+}
